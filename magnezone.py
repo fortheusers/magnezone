8b9209f3f21858ac03be8466a6775ff9f953aeb8
vgmoose <me@vgmoose.com>
2025-03-19 13:01:47 -0400

    initial commit, pull down both repos, output repo.json

---
+++ magnezone.py
@@ -0,0 +1,55 @@
+#!/usr/local/env python3
+
+import sys, os
+import json
+import requests
+
+REPO1 = "https://switch-hbas-repo.b-cdn.net/repo.json"
+REPO2 = "https://switchbru.com/appstore/repo.json"
+
+def refresh():
+	print("Refreshing repos...")
+	# if one of our repo's doesn't exist, re-download it
+	for idx, repo in enumerate([REPO1, REPO2]):
+		if not os.path.exists(f"repo{idx + 1}.json"):
+			print(f"Repo {idx + 1} doesn't exist, re-downloading...")
+			with open(f"repo{idx + 1}.json", "w") as f:
+				f.write(requests.get(repo).text)
+	# load up the package data from the first one
+	data = {}
+	with open("repo1.json", "r") as f:
+		data = json.loads(f.read())
+	packageKeys = set()
+	if "packages" in data:
+		for package in data["packages"]:
+			if "name" in package: # this field should be mandatory
+				packageKeys.add(package["name"])
+	else:
+		print("No packages in repo1.json")
+		data["packages"] = []
+	# now for the data in the second repo, do the same but skip any duplicates
+	duplicates = set()
+	with open("repo2.json", "r") as f:
+		data2 = json.loads(f.read())
+		if "packages" in data2:
+			for package in data2["packages"]:
+				if "name" in package:
+					if package["name"] in packageKeys:
+						duplicates.add(package["name"])
+					else:
+						data["packages"].append(package)
+					
+		else:
+			print("No packages in repo2.json")
+	# save the updated data
+	with open("repo.json", "w") as f:
+		f.write(json.dumps(data, indent=4))
+	print("BZZZZT! Refresh complete, merged repo.json is ready")
+	if len(duplicates) > 0:
+		print(f"Excluded these duplicate packages from repo2: {duplicates}")
+	
+	# TODO: Clear cache on CDN
+
+print("Magnezone: Repo Merger v0.1")
+# TODO: add commands to clear repo1/repo2/both
+refresh()
